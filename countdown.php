63dfa3cdf911300f809877c342fb3893d1151431
Ufuk Avcu <info@jooag.de>
2014-11-26 18:09:26 +0100

    Event & JS Problems solved

--- countdown.php
+++ countdown.php
@@ -1,61 +1,54 @@
 <?php
 /**
- * @version		3.0 Alpha
- * @for			Joomla 3.3+
- * @package		JooAg Countdown
- * @author		Joomla Agentur - http://www.joomla-agentur.de
- * @copyright		Copyright (c) 2009 - 2015 Joomla-Agentur All rights reserved.
- * @license     	GNU General Public License version 2 or later;
- * @description		A small Plugin to Calculate the Days for a specific Date
- * @usage		Put somewhere in your Content or Module following Snippet - {countdown}02 December 2014 18:45:00{/countdown}
- */
+Joomla Agency
+A small Plugin to Calculate the Days for a specific Date
+Usage:
+Put somewhere in your Content or Module following Snippet - {countdown}02 December 2014 18:45:00{/countdown}
+**/
 
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
 jimport( 'joomla.plugin.plugin');
 
-class plgContentCountdown extends JPlugin
+class PlgContentCountdown extends JPlugin
 {
 	public function __construct(&$subject, $config)
 	{
 		parent::__construct($subject, $config);
 	}
 	
-	public function onContentPrepare($context, &$article, &$params, $page=0 )
+	public function onContentPrepare($context, &$article, &$params, $page = 0)
 	{
-		// Performance Check
+		// simple performance check to determine whether plugin should process further
 		if ( JString::strpos( $article->text, '{countdown}' ) === false ) {
 			return true;
 		}
-	
-		// Regular expression for the plugin
+
+		// define the regular expression for the plugin
 		$regex = "#{countdown}(.*?){/countdown}#s";
 
-		// Replacement of {countdown}xxx{/countdown}
+		// perform the replacement
 		$article->text = preg_replace_callback( $regex, array(&$this,'plgCountdownDTN_replacer'), $article->text );
-		return true;
+		$article->introtext = preg_replace_callback( $regex, array(&$this,'plgCountdownDTN_replacer'), $article->text );
 	}
 	
-	protected function plgCountdownDTN_replacer ( &$matches) 
+	protected function plgCountdownDTN_replacer (&$matches) 
 	{
 		$date = $matches[1];
-		
-		//Javascript Part
+		$doc = JFactory::getDocument();	
 		JHtml::_('jquery.framework');
-		$doc = JFactory::getDocument();
 		$doc->addScript(JURI::root().'plugins/content/countdown/countdown.js');
-		$doc->addStyleDeclaration( "#countdown span {font-size: 20px;font-weight:600;color:red;}" );	
+		$doc->addStyleDeclaration( ".countdown span {font-size: 20px;font-weight:600;color:red;}" );	
+		$randomId = rand();
 		
 		$javascript = 'jQuery(document).ready(function () {';
-		$javascript .= 'jQuery("#countdown").countdown({';
+		$javascript .= 'jQuery("#countdown'.$randomId.'").countdown({';
 		$javascript .= 'date: "'.$date.'",';
 		$javascript .= 'format: "on"';
 		$javascript .= '});';
 		$javascript .= '});';
 		$doc->addScriptDeclaration($javascript);
-		
-		//Output
-		$days = '<div id="countdown">
+		$days = '<div class="countdown" id="countdown'.$randomId.'">
 					<span class="days">00</span>
 					<span class="timeRefDays">Tage</span> -
 					<span class="hours">00</span>:<span class="minutes">00</span>:<span class="seconds">00</span>
@@ -76,6 +69,4 @@ class plgContentCountdown extends JPlugin
 		
 		return $days;
 	}
-	
-
 }
