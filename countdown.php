04e881c0fa1d8f4e0c791683eeb69050aa246adc
Ufuk Avcu <info@jooag.de>
2014-11-27 09:52:14 +0100

    Structure changes

--- countdown.php
+++ countdown.php
@@ -1,17 +1,15 @@
 <?php
 /**
-* @version 3.0 Alpha 	+Joomla Agency
-* @for Joomla 3.3+ 	+A small Plugin to Calculate the Days for a specific Date
-* @package JooAg Countdown 	+Usage:
-* @author Joomla Agentur - http://www.joomla-agentur.de 	+Put somewhere in your Content or Module following Snippet - {countdown}02 December 2014 18:45:00{/countdown}
-* @copyright Copyright (c) 2009 - 2015 Joomla-Agentur All rights reserved.
-* @license GNU General Public License version 2 or later; 	
-* @description A small Plugin to Calculate the Days for a specific Date 	
-* @usage Put somewhere in your Content or Module following Snippet - {countdown}02 December 2014 18:45:00{/countdown} 	
-*/
-
+ * @version 3.0 Alpha +Joomla Agency
+ * @for Joomla 3.3+ +A small Plugin to Calculate the Days for a specific Date
+ * @package JooAg Countdown +Usage:
+ * @author Joomla Agentur - http://www.joomla-agentur.de +Put somewhere in your Content or Module following Snippet - {countdown}02 December 2014 18:45:00{/countdown}
+ * @copyright Copyright (c) 2009 - 2015 Joomla-Agentur All rights reserved.
+ * @license GNU General Public License version 2 or later;
+ * @description A small Plugin to Calculate the Days for a specific Date
+ * @usage Put somewhere in your Content or Module following Snippet - {countdown}02 December 2014 18:45:00{/countdown}
+ */
 defined( '_JEXEC' ) or die( 'Restricted access' );
-
 jimport( 'joomla.plugin.plugin');
 
 class PlgContentCountdown extends JPlugin
@@ -27,42 +25,30 @@ class PlgContentCountdown extends JPlugin
 		if ( JString::strpos( $article->text, '{countdown}' ) === false ) {
 			return true;
 		}
-
+		
+		$doc = JFactory::getDocument();
+		JHtml::_('jquery.framework');
+		$doc->addScript(JURI::root().'plugins/content/countdown/countdown.js');
+		$doc->addStyleDeclaration($this->params->def('countdowncss'));
+				
 		// Regular expression
 		$regex = "#{countdown}(.*?){/countdown}#s";
-
+		
 		// Replacement of {countdown}xxx{/countdown}
 		$article->text = preg_replace_callback( $regex, array(&$this,'plgCountdownDTN_replacer'), $article->text );
 		$article->introtext = preg_replace_callback( $regex, array(&$this,'plgCountdownDTN_replacer'), $article->text );
 	}
 	
-	protected function plgCountdownDTN_replacer (&$matches) 
+	protected function plgCountdownDTN_replacer (&$matches)
 	{
-		
 		$date = $matches[1];
-		$doc = JFactory::getDocument();	
-		JHtml::_('jquery.framework');
-		$doc->addScript(JURI::root().'plugins/content/countdown/countdown.js');
-		$doc->addStyleDeclaration($this->params->def('countdowncss'));	
-		$randomId = rand();
-		
-		$javascript = 'jQuery(document).ready(function () {';
-		$javascript .= 'jQuery("#countdown'.$randomId.'").countdown({';
-		$javascript .= 'date: "'.$date.'",';
-		$javascript .= 'format: "on"';
-		$javascript .= '});';
-		$javascript .= '});';
-		$doc->addScriptDeclaration($javascript);
-		
 		$htmlOutput = '
-		<span class="days">00</span> <span class="timeRefDays">days</span> 
-		<span class="hours">00</span> <span class="timeRefHours">hours</span> 
-		<span class="minutes">00</span> <span class="timeRefMinutes">minutes</span> 
+		<span class="days">00</span> <span class="timeRefDays">days</span>
+		<span class="hours">00</span> <span class="timeRefHours">hours</span>
+		<span class="minutes">00</span> <span class="timeRefMinutes">minutes</span>
 		<span class="seconds">00</span> <span class="timeRefSeconds">seconds</span>';
-		
-		if (!empty ($this->params->def('countdownhtml'))){$htmlOutput = $this->params->def('countdownhtml');}
-		$countdown = '<div class="countdown" id="countdown'.$randomId.'">'.$htmlOutput.'</div>';
-		
+		if (!empty ($this->params->def('countdownhtml'))) {$htmlOutput = $this->params->def('countdownhtml');}
+		$countdown = '<div class="countdown" data-countdown="'.$date.'">'.$htmlOutput.'</div>';
 		return $countdown;
 	}
 }
