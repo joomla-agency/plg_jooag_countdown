5da846fd3ea3e0b1f983c2e509aae8c40370c936
Ufuk Avcu <info@jooag.de>
2014-11-27 12:08:14 +0100

    Update countdown.php

--- countdown.php
+++ countdown.php
@@ -8,46 +8,41 @@
  * @license GNU General Public License version 2 or later;
  * @description A small Plugin to Calculate the Days for a specific Date
  * @usage Put somewhere in your Content or Module following Snippet - {countdown}02 December 2014 18:45:00{/countdown}
+ * @thanksto Thanks to Guido De Gobbis from http://joomtools.de for his great contributions!
  */
 defined( '_JEXEC' ) or die( 'Restricted access' );
 jimport( 'joomla.plugin.plugin');
-
 class PlgContentCountdown extends JPlugin
 {
 	public function __construct(&$subject, $config)
 	{
 		parent::__construct($subject, $config);
 	}
-	
 	public function onContentPrepare($context, &$article, &$params, $page = 0)
 	{
-		// Performance Check
+// Performance Check
 		if ( JString::strpos( $article->text, '{countdown}' ) === false ) {
 			return true;
 		}
-		
 		$doc = JFactory::getDocument();
 		JHtml::_('jquery.framework');
 		$doc->addScript(JURI::root().'plugins/content/countdown/countdown.js');
 		$doc->addStyleDeclaration($this->params->def('countdowncss'));
-				
-		// Regular expression
+// Regular expression
 		$regex = "#{countdown}(.*?){/countdown}#s";
-		
-		// Replacement of {countdown}xxx{/countdown}
+// Replacement of {countdown}xxx{/countdown}
 		$article->text = preg_replace_callback( $regex, array(&$this,'plgCountdownDTN_replacer'), $article->text );
 		$article->introtext = preg_replace_callback( $regex, array(&$this,'plgCountdownDTN_replacer'), $article->text );
 	}
-	
 	protected function plgCountdownDTN_replacer (&$matches)
 	{
 		$date = $matches[1];
-		$htmlOutput = '
+		$_htmlOutput = '
 		<span class="days">00</span> <span class="timeRefDays">days</span>
 		<span class="hours">00</span> <span class="timeRefHours">hours</span>
 		<span class="minutes">00</span> <span class="timeRefMinutes">minutes</span>
 		<span class="seconds">00</span> <span class="timeRefSeconds">seconds</span>';
-		if (!empty ($this->params->def('countdownhtml'))) {$htmlOutput = $this->params->def('countdownhtml');}
+		$htmlOutput = $this->params->get('countdownhtml', $_htmlOutput);
 		$countdown = '<div class="countdown" data-countdown="'.$date.'">'.$htmlOutput.'</div>';
 		return $countdown;
 	}
